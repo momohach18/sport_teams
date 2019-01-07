f3dd143cf74d34a604b1d886de45eea8dc8d5158
Mohamed Amine HACHFI <hachfi.mohamed.amine@gmail.com>
2019-01-07 10:09:47 +0100

    Initial Commit

---
+++ Leagues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leagues
+//
+//  Created by Macbook on 07/01/2019.
+//  Copyright © 2019 MAH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
